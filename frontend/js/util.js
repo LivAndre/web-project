000357265b27d840bdb7bd88eed2770b1ec18591
mitsuweee <142657685+mitsuweee@users.noreply.github.com>
2023-12-18 18:11:52 +0800

    CARTID

--- frontend/js/util.js
+++ frontend/js/util.js
@@ -10,4 +10,27 @@ const getUrl = (env)=>{
     }
 
     return url
+
 }
+// GENERATE CART ID REFERENCE
+function generateCartId() {
+    const timestamp = new Date().getTime(); // Get current timestamp
+    const cartId = `cart_${timestamp}`;
+    return cartId;
+  }
+  window.onload = function () {
+    let cartId = "";
+    const cartIdContainerHomepage = document.getElementById('cartIdContainer');
+    if (cartIdContainerHomepage.hasAttribute('value')){
+        cartId = cartIdContainerHomepage.getAttribute('value');
+        if (!(cartId !== null && cartId.trim() !== '')) {
+            cartId = generateCartId();
+            sessionStorage.setItem('cartId', cartId);
+        }
+        console.log("WALANG LAMAN"); 
+    }
+    
+    if (cartIdContainerHomepage) {
+        cartIdContainerHomepage.setAttribute('value',cartId);
+    }
+  };
